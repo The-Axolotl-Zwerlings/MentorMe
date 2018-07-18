3cf2a610846233ad2c69091f2afa3b2648a142b6
Taylor Murray <taylormurray@fb.com>
2018-07-18 09:47:21 -0700

    all models made

---
+++ MentorMe/New Group/AppointmentModel.h
@@ -0,0 +1,13 @@
+//
+//  AppointmentModel.h
+//  MentorMe
+//
+//  Created by Taylor Murray on 7/18/18.
+//  Copyright © 2018 Taylor Murray. All rights reserved.
+//
+
+#import "PFObject.h"
+
+@interface AppointmentModel : PFObject <PFSubclassing>
+
+@end
---
+++ MentorMe/New Group/AppointmentModel.m
@@ -0,0 +1,17 @@
+//
+//  AppointmentModel.m
+//  MentorMe
+//
+//  Created by Taylor Murray on 7/18/18.
+//  Copyright © 2018 Taylor Murray. All rights reserved.
+//
+
+#import "AppointmentModel.h"
+
+@implementation AppointmentModel
+
++(nonnull NSString *)parseClassName{
+    return @"AppointmentModel";
+}
+
+@end
